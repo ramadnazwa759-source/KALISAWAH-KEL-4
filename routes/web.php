33a2b828d71913be16469cfa88586329a46ab5db
rasyaaprilia13 <rassyaapr13@gmail.com>
2026-04-02 11:59:11 +0700

    rasya commit

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    return view('home');
 });
+
+
