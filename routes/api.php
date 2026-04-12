5dd5aaa56f223fe3dbb817b4e55b85df67c20a27
rasyaaprilia13 <rassyaapr13@gmail.com>
2026-04-12 11:49:02 +0700

    metrics sistem terdistribusi fix

--- routes/api.php
+++ routes/api.php
@@ -8,24 +8,6 @@ Route::get('/test', function () {
     return "API WORKING";
 });
 
-// route metrics (tugas sistem terdistribusi)
-Route::get('/metrics', function () {
-    $memory = memory_get_usage();
-
-    $metrics = "
-# HELP app_status Application status
-# TYPE app_status gauge
-app_status 1
-
-# HELP memory_usage Memory usage in bytes
-# TYPE memory_usage gauge
-memory_usage $memory
-";
-
-    return response($metrics, 200)
-        ->header('Content-Type', 'text/plain');
-});
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+// route metrics (tugas sistem terdistribusi)
+Route::get('/metrics', function () {
+    return response("
+# HELP app_status Application status
+# TYPE app_status gauge
+app_status 1
+
+# HELP memory_usage Memory usage in bytes
+# TYPE memory_usage gauge
+memory_usage " . memory_get_usage()
+    , 200)
+    ->header('Content-Type', 'text/plain; version=0.0.4');
+});
 
 Route::get('/', function () {
     return view('welcome');
