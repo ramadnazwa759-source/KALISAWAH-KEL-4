9aadbc0b689e7084ac03eafa1d202613b3ffd270
NAZWA RIDEV <ramadnazwa759@gmail.com>
2026-06-07 02:02:24 +0700

    push terbaru

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role')->enum(['admin', 'pemilik']);
+            $table->enum('role', ['admin', 'pemilik']);
             $table->timestamps();
         });
     }
